93503c3b6dd283df3238c387cfa47aa001df00d6
Rohit27082000 <ry609898@gmail.com>
2023-12-16 19:23:30 +0530

    Home page

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-    let currentCard = 0; // Start from the first card
+let currentCard = 0; // Start from the first card
 
-    function showNextCard() {
-    const cardContainer = document.getElementById('card-container'); // Corrected the id
-    const cards = document.querySelectorAll('.sec3card');
+function showNextCard() {
+  const cardContainer = document.getElementById("card-container"); // Corrected the id
+  const cards = document.querySelectorAll(".sec3card");
 
-    if (currentCard < cards.length - 1) {
-        cards[currentCard].style.display = 'none';
-        currentCard++;
-        cards[currentCard].style.display = 'block'; // Use 'block' or 'inline-block' to show the card
-        cardContainer.scrollLeft += cards[currentCard].offsetWidth;
-    }
-    }
+  if (currentCard < cards.length - 1) {
+    cards[currentCard].style.display = "none";
+    currentCard++;
+    cards[currentCard].style.display = "block"; // Use 'block' or 'inline-block' to show the card
+    cardContainer.scrollLeft += cards[currentCard].offsetWidth;
+  }
+}
 
-    function showPreviousCard() {
-    const cardContainer = document.getElementById('card-container'); // Corrected the id
-    const cards = document.querySelectorAll('.sec3card');
+function showPreviousCard() {
+  const cardContainer = document.getElementById("card-container"); // Corrected the id
+  const cards = document.querySelectorAll(".sec3card");
 
-    if (currentCard > 0) {
-        cards[currentCard].style.display = 'none';
-        currentCard--;
-        cards[currentCard].style.display = 'block'; // Use 'block' or 'inline-block' to show the card
-        cardContainer.scrollLeft -= cards[currentCard].offsetWidth;
-    }
-    }
+  if (currentCard > 0) {
+    cards[currentCard].style.display = "none";
+    currentCard--;
+    cards[currentCard].style.display = "block"; // Use 'block' or 'inline-block' to show the card
+    cardContainer.scrollLeft -= cards[currentCard].offsetWidth;
+  }
+}
